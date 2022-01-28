8d54414e047b9d818eae05546aa4af58fb3c7916
1198159 <jeffastedman@gmail.com>
2022-01-28 12:52:09 -0800

    command fix

--- RobotLibrary/src/main/java/com/technototes/library/command/CommandScheduler.java
+++ RobotLibrary/src/main/java/com/technototes/library/command/CommandScheduler.java
@@ -63,10 +63,10 @@ public final class CommandScheduler {
     }
 
     public CommandScheduler scheduleOnce(Command command) {
-        return schedule(command.andThen(()->commandMap.remove(command)));
+        return schedule(command);
     }
     public CommandScheduler scheduleOnceForState(Command command, CommandOpMode.OpModeState state) {
-        return scheduleForState(command.andThen(()->commandMap.remove(command)), state);
+        return scheduleForState(command, state);
     }
 
     public CommandScheduler scheduleInit(Command command, BooleanSupplier supplier) {
