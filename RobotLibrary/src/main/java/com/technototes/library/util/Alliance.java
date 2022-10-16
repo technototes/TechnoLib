2694b997b494bd70fa20a53db215cef927a0a2dd
Kevin Frei <kevinfrei@hotmail.com>
2022-10-16 09:53:16 -0700

    More minor doc updates

--- RobotLibrary/src/main/java/com/technototes/library/util/Alliance.java
+++ RobotLibrary/src/main/java/com/technototes/library/util/Alliance.java
@@ -12,8 +12,17 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
  * An enumeration to specify which alliance the bot is on (Red, Blue, or None)
  */
 public enum Alliance {
+    /**
+     * RED alliance selector
+     */
     RED(Color.RED),
+    /**
+     * BLUE alliance selector
+     */
     BLUE(Color.BLUE),
+    /**
+     * NO ALLIANCE SELECTED
+     */
     NONE(Color.BLACK);
     Color color;
 
@@ -47,38 +56,85 @@ public enum Alliance {
         return Selector.selectOf(this, a, b);
     }
 
+    /**
+     * This feels over-engineered, but it's probably for some obnoxious Java thing,
+     * unfortunate. This lets you pick between two types based on the Alliance
+     *
+     * @param <T> The type of object to select
+     */
     public static class Selector<T> {
         private final T r, b;
 
+        /**
+         * Create a Selelector for red/blue
+         *
+         * @param red  For Red selection
+         * @param blue For Blue selection
+         */
         protected Selector(T red, T blue) {
             r = red;
             b = blue;
         }
 
+        /**
+         * Select the red or blue item based on the Alliance
+         *
+         * @param a The alliance
+         * @return The object selected
+         */
         public T select(Alliance a) {
             return a == RED ? r : b;
         }
 
+        /**
+         * Selector factory method
+         *
+         * @param red  The object to select if red
+         * @param blue The object to select if blue
+         * @param <T>  The type of red &amp; blue
+         * @return The Selector
+         */
         public static <T> Selector<T> of(T red, T blue) {
             return new Selector<>(red, blue);
         }
 
+        /**
+         * Based on Alliance, choose red or blue
+         *
+         * @param alliance The alliance
+         * @param red      The thing to choose for red
+         * @param blue     The thing to choose for blue
+         * @param <T>      The type of the red &amp; blue things
+         * @return The thing (red or blue) selected
+         */
         public static <T> T selectOf(Alliance alliance, T red, T blue) {
             return Selector.of(red, blue).select(alliance);
         }
     }
 
+    /**
+     * Get the alliance set for the OpMode passed in, if it's set in the annotation
+     *
+     * @param c The OpMode
+     * @return The alliance of the opmode
+     */
     public static Alliance get(Class<? extends OpMode> c) {
         if (c.isAnnotationPresent(Red.class)) return RED;
         if (c.isAnnotationPresent(Blue.class)) return BLUE;
         return NONE;
     }
 
+    /**
+     * Not sure what this is for. Something for annotation processing? No idea :/
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(TYPE)
     public @interface Red {
     }
 
+    /**
+     * Not sure what this is for. Something for annotation processing? No idea :/
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(TYPE)
     public @interface Blue {
