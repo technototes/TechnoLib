96449ee343f9acaf20ce67ac67aaffa12e938b55
Kevin Frei <kevinfrei@hotmail.com>
2022-10-16 15:02:25 -0700

    Docs only in docs branch

---
+++ docs/Vision/package-search-index.js
@@ -0,0 +1,6 @@
+packageSearchIndex = [
+  { l: 'All Packages', u: 'allpackages-index.html' },
+  { l: 'com.technototes.vision.hardware' },
+  { l: 'com.technototes.vision.subsystem' },
+];
+updateSearchResults();
