5db0853630be9f9c640817446bac88fc8e6741f0
Kevin Frei <kevinfrei@hotmail.com>
2022-12-06 11:06:19 -0800

    Sliced & diced scripts a little bit

--- RobotLibrary/src/main/java/com/technototes/library/util/MathUtils.java
+++ RobotLibrary/src/main/java/com/technototes/library/util/MathUtils.java
@@ -111,11 +111,11 @@ public class MathUtils {
      * @return
      */
     public static double map(
-            double x,
-            double in_min,
-            double in_max,
-            double out_min,
-            double out_max
+        double x,
+        double in_min,
+        double in_max,
+        double out_min,
+        double out_max
     ) {
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
