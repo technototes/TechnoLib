562690804c71fd349b2c97480fd025f2481ba97e
Kevin Frei <kevinfrei@hotmail.com>
2023-10-19 22:11:24 -0700

    Deprecated the Paramater*Command classes, and made SimpleRequiredCommand also handle a parameter

--- RobotLibrary/src/main/java/com/technototes/library/command/ParameterCommand.java
+++ RobotLibrary/src/main/java/com/technototes/library/command/ParameterCommand.java
@@ -4,6 +4,12 @@ import com.technototes.library.subsystem.Subsystem;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
+/**
+ * Use SimpleCommand: It can handle a single Function (Runnable)
+ * or a F(T) (Consumer<T>)
+ * @param <U>
+ */
+@Deprecated
 public class ParameterCommand<U> implements Command {
 
     U param;
