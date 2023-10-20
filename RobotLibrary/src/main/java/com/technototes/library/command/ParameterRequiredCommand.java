2e0db2b457455972845262c61c85811a3a8e417c
Kevin Frei <kevinfrei@hotmail.com>
2023-10-19 22:11:24 -0700

    Deprecated the Paramater*Command classes, and made SimpleRequiredCommand also handle a parameter

--- RobotLibrary/src/main/java/com/technototes/library/command/ParameterRequiredCommand.java
+++ RobotLibrary/src/main/java/com/technototes/library/command/ParameterRequiredCommand.java
@@ -3,6 +3,14 @@ package com.technototes.library.command;
 import com.technototes.library.subsystem.Subsystem;
 import java.util.function.BiConsumer;
 
+/**
+ * Use SimplerRequiredCommand instead of this. It's more flexible:
+ * It works with or without a parameter, so it needed renamed...
+ *
+ * @param <T>
+ * @param <U>
+ */
+@Deprecated
 public class ParameterRequiredCommand<T extends Subsystem, U> implements Command {
 
     T sub;
--- RobotLibrary/src/main/java/com/technototes/library/command/SimpleRequiredCommand.java
+++ RobotLibrary/src/main/java/com/technototes/library/command/SimpleRequiredCommand.java
@@ -1,6 +1,7 @@
 package com.technototes.library.command;
 
 import com.technototes.library.subsystem.Subsystem;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class SimpleRequiredCommand<T extends Subsystem> implements Command {
@@ -10,8 +11,15 @@ public class SimpleRequiredCommand<T extends Subsystem> implements Command {
 
     public SimpleRequiredCommand(T s, Consumer<T> m) {
         super();
-        s = sub;
+        sub = s;
         method = m;
+        addRequirements(sub);
+    }
+
+    public <U> SimpleRequiredCommand(T s, BiConsumer<T, U> m, U arg) {
+        super();
+        sub = s;
+        method = subsys -> m.accept(subsys, arg);
         addRequirements(sub);
     }
 
